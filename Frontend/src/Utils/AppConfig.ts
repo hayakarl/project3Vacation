f170869061869aabe2d03e746ec7e0032d7c38e1
haya karl <khayakyony@gmail.com>
2024-09-09 07:34:42 +0300

    aws

--- Frontend/src/Utils/AppConfig.ts
+++ Frontend/src/Utils/AppConfig.ts
@@ -1,5 +1,6 @@
 class AppConfig {
   public readonly backendUrl = 'http://localhost:4000/api/';
+//   public readonly backendUrl = 'http://52.34.223.202:4000/api/';
 }
 
 export const appConfig = new AppConfig();
