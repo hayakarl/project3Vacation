6d5e1e0d09442711c736b1894f99cd35cd64e79d
haya karl <khayakyony@gmail.com>
2024-09-09 22:16:55 +0300

    update

--- Frontend/src/Utils/AppConfig.ts
+++ Frontend/src/Utils/AppConfig.ts
@@ -1,6 +1,18 @@
 class AppConfig {
-  public readonly backendUrl = 'http://localhost:4000/api/';
-//   public readonly backendUrl = 'http://52.34.223.202:4000/api/';
+  //   public readonly backendUrl = 'http://localhost:4000/api/';
+  public readonly backendUrl = 'http://16.170.52.42:4040/api/';
 }
 
 export const appConfig = new AppConfig();
+
+// class DevelopmentConfig {
+//   public readonly backendUrl = 'http://localhost:4000/api/';
+// }
+
+// class ProductionConfig {
+//   public readonly backendUrl = 'http://52.34.223.202:4000/api/';
+// }
+
+// // const appConfig = process.env.REACT_APP_ENVIRONMENT === 'development' ? new DevelopmentConfig() : new ProductionConfig();
+
+// export default appConfig;
